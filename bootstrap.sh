9b9fbf143c7139fafa80f5d4c83d3403285fe533
hejamu <info@jaegerhenrik.de>
2020-10-20 05:04:52 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,11 @@ iocage fetch -P spectrum-protect.json
 
 iocage stop spectrum-protect
 
+# Change these to point to the data you want to backup
 iocage fstab -a spectrum-protect '/mnt/home /home nullfs rw 0 0'
 iocage fstab -a spectrum-protect '/mnt/gruppen /gruppen nullfs rw 0 0'
+
+# Change these to point to the config files (see README.md)
 iocage fstab -a spectrum-protect '/mnt/freenasswap/iocage/templates/adsm /compat/linux/etc/adsm nullfs rw 0 0'
 iocage fstab -a spectrum-protect '/mnt/freenasswap/iocage/templates/tsm /compat/linux/etc/tsm nullfs rw 0 0'
 
